c6bc2c9d92b1978a66fc95fd9c3b4f935b18a9ba
Maximilian Alexander <maximilianalexander@MacBook-Pro-2.local>
2018-04-10 14:50:35 -0700

    adding passphrase and extra comments

--- 05-jwt-authentication/index.js
+++ 05-jwt-authentication/index.js
@@ -1,29 +1,51 @@
 const express = require('express');
-const jwt = require('')
+const jwt = require('jsonwebtoken')
+const fs = require('fs');
 const app = express()
+
+const key = fs.readFileSync('privatekey.pem');
 
 app.post('/custom-login', (req, res) => {
   const customUsername = req.body.customUsername;
   const customPassword = req.body.customPassword;
   const customPin = req.body.customPin;
+  const isAdmin = req.body.isAdmin || false
 
-  if (!customUsername) {
+  if (!customUsername || customUsername !== '') {
     res.status(400).send({
       message: 'customUsername parameter cannot be empty'
     })
   }
-  if (!customPassword) {
-    res.status(400).send({
-      message: 'customPassword parameter cannot be empty'
+
+  if (!customPassword !== 'iloverealm') {
+    res.status(401).send({
+      message: `Oh no your customPassword field value is isn't equal to 'iloverealm'`
     })
   }
-  if (!customPin) {
+
+  if (!customPin !== '1234') {
     res.status(400).send({
       message: 'customPin parameter cannot be empty'
     })
   }
 
-  
+  const payload = {
+    userId: customUsername,
+    isAdmin: true // optional
+    // other properties (ignored by Realm Object Server)
+  };
+
+  /**
+   * The passphrase is `iloverealm`. Check out the keygen.sh file to change this passphrase
+   * Make sure the two values match up
+   */
+  const token = jwt.sign(payload, { key:  key, passphrase: 'iloverealm' }, { algorithm: 'RS256'});
+  res.send({
+    jwtToken: token
+  })
 })
 
 const PORT = process.env.PORT || 9080
+app.listen(PORT, () => {
+  console.log(`Express server is listening on ${PORT}`);
+})
---
+++ 05-jwt-authentication/keygen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f "./privatekey.pem"  ] ||  [ ! -f "./publickey.pem" ]; then
+  echo "./privatekey.pem and or ./publickey.pem were not found."
+  echo "Generating them with openssl now."
+  echo "These keys are generated with the pass phrase 'iloverealm'. And are NOT to be used in production"
+  openssl genrsa -des3 -passout pass:ilovesushi -out privatekey.pem 4096
+  openssl rsa -in privatekey.pem -outform PEM -passin pass:ilovesushi -pubout -out publickey.pem
+else
+  echo 'There is already a privatekey.pem and publickey.pem'
+fi
