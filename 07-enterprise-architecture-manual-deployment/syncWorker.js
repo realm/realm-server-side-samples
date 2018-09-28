40fb0598c05241f97cb3b7853ab25e9153967cd6
mgeerling <mbgeerling@gmail.com>
2018-09-28 16:30:37 -0700

    Added syncworker and coreservices example with file logger for enterprise deployment

---
+++ 07-enterprise-architecture-manual-deployment/syncWorker.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const ros = require('realm-object-server');
+const rose = require('realm-object-server-enterprise');
+const os = require('os');
+
+process.env.FEATURE_TOKEN = "..."; // Insert your Feature Token in here
+
+function getIPv4Address(interfaceName) {
+    return os.networkInterfaces()[interfaceName].filter(iface => iface.family === 'IPv4')[0].address;
+}
+
+const server = new ros.BasicServer();
+
+const port = process.env.PORT || 9080; // This is a unique port for the server shell which coordinates the various services
+const label = process.env.SYNC_LABEL || "default"; // This label needs to be unique for each group of sync-workers
+const syncId = process.env.SYNC_ID || os.hostname(); // SYNC_ID must be unique for every sync-worker. We use the hostname here for convenience but takes a string
+const syncPort = process.env.SYNC_PORT || 7800; // This is the port ROS Core Services will route Realm Sync Clients to
+const privateKeyPath = path.join(__dirname, 'data/keys/auth.key');
+const publicKeyPath = path.join(__dirname, 'data/keys/auth.pub');
+
+const discovery = new rose.ConsulDiscovery({
+    consulHost: process.env.CONSUL_HOST || "127.0.0.1",
+    consulPort: process.env.CONSUL_PORT || "8500"
+});
+
+const startConfig = {
+    services: [
+        new ros.LogService(),
+        new rose.ReplicatedSyncService({
+            consul: discovery.consul,
+            dataPath: path.join(__dirname, 'data'),
+            featureToken: process.env.FEATURE_TOKEN,
+            id: syncId,
+            label: label,
+            listenAddress: getIPv4Address('eth0'),
+            listenPort: Number(syncPort),
+            logLevel: "debug",
+            publicKeyPath
+        })
+    ],
+    privateKeyPath,
+    publicKeyPath,
+    port,
+    discovery,
+    dataPath: path.join(__dirname, 'data'),
+    logger: new ros.FileConsoleLogger(path.join(__dirname, '../log.txt'), 'all', {
+        file: {
+            timestamp: true,
+            level: 'debug'
+        },
+        console: {
+            level: 'debug'
+        }
+    })
+};
+
+server.start(startConfig).then(() => {
+    console.log(`Your server is started at ${server.address}`);
+})
+.catch(err => {
+    console.error('There was an error starting your server');
+    console.error(err);
+});
